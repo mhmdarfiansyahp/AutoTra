5696adf124650e2a6d45cd1c4550c0045345b264
mhmdarfiansyahp <mhmdarfiansyahp@gmail.com>
2023-11-30 18:51:05 +0700

    login

--- AutoTra/Program.cs
+++ AutoTra/Program.cs
@@ -3,21 +3,31 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Configure the services for session
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Sesuaikan dengan kebutuhan Anda
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-app.UseRouting();
+// Enable session before routing and authorization
+app.UseSession();
 
+app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
