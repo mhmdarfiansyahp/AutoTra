39747ce7127edbabd2062f43f295d8f6a6f09a85
mhmdarfiansyahp <mhmdarfiansyahp@gmail.com>
2023-12-15 08:18:13 +0700

    ini

--- AutoTra/Controllers/DaftarPengajuanController.cs
+++ AutoTra/Controllers/DaftarPengajuanController.cs
@@ -8,12 +8,14 @@ namespace AutoTra.Controllers
         private readonly DaftarPengajuan Dpengajuanrepositori;
         private readonly Mobil mobilrepository;
         private readonly Dosen dsnrepository;
+        private readonly PIC picrepository;
 
         public DaftarPengajuanController(IConfiguration configuration)
         {
             Dpengajuanrepositori = new DaftarPengajuan(configuration);
             mobilrepository = new Mobil(configuration);
             dsnrepository = new Dosen(configuration);
+            picrepository = new PIC(configuration);
         }
         public IActionResult Index()
         {
@@ -30,12 +32,15 @@ namespace AutoTra.Controllers
             var pengajuan = Dpengajuanrepositori.getAllData(npk);
             var mobil = mobilrepository.getAllData();
             var dsn = dsnrepository.getAllData();
+            var pic = picrepository.getAllData();
 
             var dsndictinory = dsn.ToDictionary(d => d.npk, d => d.nama);
             var mobildictionary = mobil.ToDictionary(m => m.id_mobil, m => m.nama);
+            var Picdictionary = pic.ToDictionary(p => p.nim, p =>  p.nama);
 
             ViewBag.dsndictinary = dsndictinory;
             ViewBag.Mobildictinary = mobildictionary;
+            ViewBag.picdictinary = Picdictionary;
 
             return View(pengajuan);
         }
